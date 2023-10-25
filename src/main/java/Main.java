9e652bcce7adbac4a114fa88820505dc6be4aa6f
Boscoe <garrett.boscoe@fiserv.com>
2023-10-25 14:57:38 -0400

    First exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,28 @@
-public class Main {
+
+public class Main{
+
     public static void main(String[] args) {
         ex1();
-        // ex2();
-        // ex3();
-        // ex4();
-        // ex5();
+        //ex2();
+        //ex3();
+        //ex4();
+        //ex5();
     }
 
     private static void ex1() {
-        System.out.println("Todo...");
+        var p1 = new Person();
+        var p2 = new Person("Jon", "Smith");
+        var p3 = new Person("Mary", "Jane", (byte) 12, 123456789);
+
+        System.out.println(p1.speak());
+        System.out.println(p2.speak());
+        System.out.println(p3.speak());
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+        System.out.println("Person count: " + Person.getPersonCount());
     }
 
     private static void ex2() {
---
+++ src/main/java/Person.java
@@ -0,0 +1,55 @@
+public class Person {
+    private String  firstName;
+    private String lastName;
+    private byte age;
+    private int phoneNum;
+    private static int count = 0;
+
+    public Person(){
+        this.firstName = " ";
+        this.lastName = " ";
+        this.age = 0;
+        this.phoneNum = 0;
+        count++;
+
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = 0;
+        this.phoneNum = 0;
+        count++;
+    }
+
+    public Person(String firstName, String lastName, byte age, int phoneNum){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.phoneNum = phoneNum;
+        count++;
+    }
+
+    public String speak(){
+        if(this.firstName.equals(" ")){
+            return ("I don't have a name.");
+        }else if(this.age == 0){
+            return ("My name is " + firstName + " " + lastName);
+        }else{
+            return ("My name is " + firstName + " " + lastName + " and I am " + age + " years old.");
+        }
+    }
+
+    public String toString(){
+        if(this.firstName.equals(" ")){
+            return("N/A");
+        }else if(this.phoneNum == 0){
+            return(this.firstName + " " + this.lastName);
+        }
+        return(firstName + " " + lastName + " " + this.phoneNum);
+    }
+
+    public static int getPersonCount(){
+        return count;
+    }
+    }
